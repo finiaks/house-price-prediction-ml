06c8f4ec89fa6b763af926132135a80d4a052d0d
Akshay Prakash V <akshayprakashvenkateshprasad@gmail.com>
2026-04-19 17:54:17 +0530

    evaluate the model

--- src/main.py
+++ src/main.py
@@ -1,7 +1,10 @@
 import pandas as pd
 from sklearn.linear_model import LinearRegression
 from sklearn.preprocessing import StandardScaler
+from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
+from sklearn.model_selection import train_test_split
 import joblib as job
+
 
 #Data
 df = pd.read_csv("../data/house.csv")
@@ -17,8 +20,22 @@ x_scaled = scaler.fit_transform(x)
 #Create Model
 model = LinearRegression()
 
+#Split Data
+x_train,x_test,y_train,y_test = train_test_split(x_scaled,y, test_size = 0.3, random_state = 42)
+
 #Train Model
-model.fit(x_scaled,y)
+model.fit(x_train,y_train)
+
+y_pred = model.predict(x_test)
+
+#Metrics
+mae = mean_absolute_error(y_test,y_pred)
+mse = mean_squared_error(y_test,y_pred)
+r2 = r2_score(y_test,y_pred)
+
+print("MAE:",mae)
+print("MSE:",mse)
+print("R2 Score:",r2)
 
 #Predict
 new_df = pd.DataFrame([[1800,3,5]], columns = ["Area","Bedrooms","Age"])
